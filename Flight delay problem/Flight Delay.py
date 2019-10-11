4f408bd026abcc2fca6ea89c6f6f0289935d6bf1
younes38 <hy_mimene@esi.dz>
2019-10-11 21:24:13 +0100

    added

---
+++ Flight delay problem/Flight Delay.py
@@ -0,0 +1,149 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+import time
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from math import floor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import AdaBoostClassifier
+
+#open train & test file
+file = "Train.csv"
+df = pd.read_csv(file)
+df2 = pd.read_csv("Test.csv")
+#features to consider 
+cols = ["AC", "DATOP", "DEPSTN", "ARRSTN", "STATUS", "STD", "STA"]
+
+labels = df["target"]
+
+
+#encode strings
+l_ac = preprocessing.LabelEncoder()
+ALL = pd.concat([df["AC"], df2["AC"]], ignore_index=True)
+l_ac.fit(ALL)
+
+l_stat = preprocessing.LabelEncoder()
+l_stat.fit(df["STATUS"])
+
+l_dep = preprocessing.LabelEncoder()
+country = pd.concat([df["DEPSTN"], df["ARRSTN"], df2["DEPSTN"], df2["ARRSTN"]], ignore_index=True)
+l_dep.fit(country)
+
+l_id = preprocessing.LabelEncoder()
+ID = pd.concat([df["FLTID"], df2["FLTID"]], ignore_index=True)
+l_id.fit(ID)
+
+
+def process_data(df):
+    features = df[cols]
+    #Label Encoding
+    features["AC"] = l_ac.transform(features["AC"])
+    features["STATUS"] = l_stat.transform(features["STATUS"])
+    features["DEPSTN"] = l_dep.transform(features["DEPSTN"])
+    features["ARRSTN"] = l_dep.transform(features["ARRSTN"])
+
+    #Encode Date
+    def encode_date(date):
+        a = list(date.split('-'))
+        return (int(a[1]) - 1) * 31 + int(a[2])
+    
+    def encode_date_year(date):
+        a = list(date.split('-'))
+        return int(a[0]) - 2015
+    
+    features['day_op'] = features['DATOP'].map(encode_date)
+    features["year_op"] = features["DATOP"].map(encode_date_year)
+
+    def encode_time_std_year(date):
+        A = list(date.split(' '))
+        a = list(A[0].split('-'))
+        b = list(A[1].split(':'))
+
+        return int(a[0]) - 2015
+    
+    def encode_time_std_day(date):
+        A = list(date.split(' '))
+        a = list(A[0].split('-'))
+        b = list(A[1].split(':'))
+
+        return (int(a[1]) - 1) * 31 + int(a[2])
+    
+    def encode_time_std_time(date):
+        A = list(date.split(' '))
+        a = list(A[0].split('-'))
+        b = list(A[1].split(':'))
+
+        return int(b[0]) * 60 + int(b[1])
+    
+    
+    
+    
+    
+    
+    def encode_time_sta_year(date):
+        A = list(date.split(' '))
+        a = list(A[0].split('-'))
+        b = list(A[1].split('.'))
+
+        return int(a[0]) - 2015
+    
+    def encode_time_sta_day(date):
+        A = list(date.split(' '))
+        a = list(A[0].split('-'))
+        b = list(A[1].split('.'))
+
+        return (int(a[1]) - 1) * 31 + int(a[2])
+    
+    def encode_time_sta_time(date):
+        A = list(date.split(' '))
+        a = list(A[0].split('-'))
+        b = list(A[1].split('.'))
+
+        return int(b[0]) * 60 + int(b[1])
+    
+    features["a3"] = features["STD"].map(encode_time_std_time)
+    features["b3"] = features["STA"].map(encode_time_sta_time)
+    features.drop(columns =["STD", "STA", "DATOP"], inplace = True) 
+    
+
+    #scale data
+    scaler = StandardScaler()
+    scaler.fit(features)
+    features = scaler.transform(features)
+
+    
+    return features
+
+ftrs = process_data(df)
+
+#split data
+x_train, x_test, y_train, y_test = train_test_split(ftrs, labels, test_size = 0.1)
+clf = LinearRegression()
+
+#other algorithms
+#clf = svm.SVR(kernel = "rbf", gamma = 10.0, C = 100.0)
+#clf = GaussianNB()
+#clf  = RandomForestRegressor(max_depth=3, random_state=42, n_estimators=10000, min_samples_split = 150)
+#clf = svm.SVR(kernel = 'linear')
+#clf = linear_model.SGDRegressor(max_iter = 2000,eta0=0.00001, power_t=0.25)
+
+clf = clf.fit(x_train, y_train)
+
+X = df2[cols]
+X["DATOP"] = X["DATOP"].map(str)
+X = process_data(df2)
+ans = clf.predict(X)
+df2.drop(columns =['DATOP', 'FLTID', 'DEPSTN', 'ARRSTN', 'STD', 'STA', 'STATUS', 'AC'], inplace = True) 
+df2["target"] = ans
+df2['target'] = df2['target'].map(floor)
+
+#export results
+export_csv = df2.to_csv (r'export_dataframe.csv',index = None, header=True)
---
+++ Pre-qualifications/pre_qualification.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+from itertools import permutations
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+from sklearn import tree
+
+np.random.seed(0)
+df = pd.read_csv("Train_v2.csv")
+#columns to be considered 
+cols =['age_of_respondent','education_level','cellphone_access','job_type','location_type', 'relationship_with_head']
+
+
+def to_nb(df): #convert strings to numbers    
+    def cv2(s):
+        if s == "Yes" :
+            return 1
+        return 0
+    try:
+        col = "bank_account"
+        df[col] = list(map(cv2, df[col]))
+    except: 
+        print("err")
+    for col in cols:
+        mp = dict()
+        def convert(s) : 
+            return mp[s]
+        if df[col].dtype != np.int64  and col != "uniqueid" : 
+            col_content = df[col].values.tolist()
+            unique = set(col_content)
+            x = 0
+            for u in unique:
+                mp[u] = x
+                x += 1
+            df[col] = list(map(convert, df[col]))
+    return df
+df = to_nb(df)  
+
+features = df[cols].values
+labels = df['bank_account'].values
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size = 0.2)
+
+#scale data
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+df["age_of_respondent"].apply(np.log)
+df["household_size"].apply(np.log)
+
+
+
+clf = RandomForestClassifier(n_estimators = 1000, n_jobs = -1, max_depth = 10, min_samples_split = 200)
+clf = clf.fit(x_train, y_train)
+res = clf.score(x_test, y_test)
+print(res)
+
+#test file
+df2 = pd.read_csv("Test_v2.csv")
+df3 = pd.read_csv("Test_v2.csv")
+
+df2 = to_nb(df2)
+problem_test = df2[cols]
+
+problem_test = scaler.transform(problem_test)
+df["age_of_respondent"].apply(np.log)
+df["household_size"].apply(np.log)
+
+res = clf.predict(problem_test)
+print(res)
+
+df2["uniqueid"] = df2["uniqueid"].map(str) +" x " + df3["country"].map(str)
+print(df2["uniqueid"])
+df2["bank_account"] = list(res)
+dfs =  df2[["uniqueid", "bank_account"]]
+export_csv = dfs.to_csv (r'export_dataframe.csv',index = None, header=True)
